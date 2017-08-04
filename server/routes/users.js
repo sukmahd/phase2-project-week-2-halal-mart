6f085c81e12ae89811295f685aebd37d80131d0b
Sukmo Ronggo <hd.wijoyo@gmail.com>
2017-08-04 09:35:25 +0700

    fix login

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,9 +4,11 @@ var express = require('express');
 var router = express.Router();
 var pengguna = require('../controller/pengguna')
 var verifyAdmin = require("../helper/authAdmin")
+var authen = require('../controller/autentikasi')
 
 /* GET users listing. */
 router.get('/', verifyAdmin.authAdmin ,pengguna.getPengguna);
-router.post('/',pengguna.insertPengguna);
+router.post('/', pengguna.insertPengguna);
+router.get('/signin', authen.signin);
 
 module.exports = router;
